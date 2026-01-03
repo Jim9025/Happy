ec0812ac755584f517f5f26b21cfc785535060ca
Enoch0626 <138033476+Enoch0626@users.noreply.github.com>
2026-01-03 23:45:18 +0800

    Merge GameMenuExport features into Happy

--- launcher/src/main/java/com/Ntut/GameMenu/MainActivity.java
+++ launcher/src/main/java/com/Ntut/GameMenu/MainActivity.java
@@ -1,24 +1,32 @@
 package com.Ntut.GameMenu;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends Activity {
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
-        Button startButton = findViewById(R.id.start_game_button);
-        startButton.setOnClickListener(view -> launchUnity());
-    }
-
-    private void launchUnity() {
-        Intent intent = new Intent(this, com.unity3d.player.UnityPlayerGameActivity.class);
-        startActivity(intent);
-        finish();
+        var toUnity = findViewById(R.id.btnUnity);
+        toUnity.setOnClickListener(v -> {
+            Intent intent = new Intent(this, com.unity3d.player.UnityPlayerGameActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            startActivity(intent);
+        });
     }
 }
